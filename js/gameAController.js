fff4f02ca6ab209ad07774dc141ed5580631a3ac
oliver.widder@gmail.com <oliver.widder@gmail.com>
2014-09-15 07:54:22 +0200

    text

--- js/gameAController.js
+++ js/gameAController.js
@@ -5,11 +5,9 @@ com_geekAndPoke_Ngm1.gameAController = (function() {
     var gameAController = com_geekAndPoke_Ngm1.controllers.controller('GameAController', ['$scope', function ($scope) {
             var bubbles = {
                 nodes: [
-                    {name:"10", group:0},
-                    {name:"20", group:1}
+                    {name:"10", group:0}
                 ],
                 links: [
-                    {source:0, target:1, value:1}
                 ]
             };
 
@@ -24,8 +22,8 @@ com_geekAndPoke_Ngm1.gameAController = (function() {
                     return;
                 }
 
-                var height = 1000,
-                    width = 500;
+                var height = $(window).height(),
+                    width = $(window).width();
 
                 var color = d3.scale.category20();
 
@@ -42,8 +40,7 @@ com_geekAndPoke_Ngm1.gameAController = (function() {
                         .attr("height", height);
                 }
                 else {
-                    d3.selectAll("svg .node").remove();
-                    d3.selectAll("svg .link").remove();
+                    d3.selectAll("svg .bubble").remove();
                 }
 
                 force
@@ -51,30 +48,25 @@ com_geekAndPoke_Ngm1.gameAController = (function() {
                     .links(bubbles.links)
                     .start();
 
-                var link = svg.selectAll(".link")
-                    .data(bubbles.links)
-                    .enter().append("line")
-                    .attr("class", "link")
-                    .style("stroke-width", function(d) { return Math.sqrt(d.value); });
-
-                var node = svg.selectAll(".node")
+                var g = svg.selectAll(".bubble")
                     .data(bubbles.nodes)
-                    .enter().append("circle")
-                    .attr("r", 50)
-                    .style("fill", function(d) { return color(d.group); })
+                    .enter().append("g")
+                    .attr("class", "bubble")
+                    .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
                     .call(force.drag);
 
-                node.append("title")
-                    .text(function(d) { return d.name; });
+                var bubbles = g
+                    .append("circle")
+                    .attr("r", Math.min(width, height) / 3)
+                    .style("fill", function(d) { return color(d.group); });
+
+                var bubblesText = g.append("text")
+                    .text(function(d) { return d.name; })
+                    .style("font-size", function(d) { return Math.min(2 * d.r, (2 * d.r - 8) / this.getComputedTextLength() * 24) + "px"; })
+                    .attr("dy", ".35em");
 
                 force.on("tick", function() {
-                    link.attr("x1", function(d) { return d.source.x; })
-                        .attr("y1", function(d) { return d.source.y; })
-                        .attr("x2", function(d) { return d.target.x; })
-                        .attr("y2", function(d) { return d.target.y; });
-
-                    node.attr("cx", function(d) { return d.x; })
-                        .attr("cy", function(d) { return d.y; });
+                    g.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
                 });
             });
         };
