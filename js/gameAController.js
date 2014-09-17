0cb0f31cff06d749708b79cb518bdcf9a17fc6a9
oliver.widder@gmail.com <oliver.widder@gmail.com>
2014-09-17 19:35:32 +0200

    holy sh.... It works!!!

--- js/gameAController.js
+++ js/gameAController.js
@@ -3,65 +3,58 @@ com_geekAndPoke_Ngm1.gameAController = (function() {
     var util = com_geekAndPoke_Ngm1.util;
 
     var isInNewBubblePhase = true;
+    var force, svg, g, width, height, middleX;
 
     var gameAController = com_geekAndPoke_Ngm1.controllers.controller('GameAController', ['$scope', function ($scope) {
             var currentVal = 0;
 
-            function createNextBubble() {
+            function newBubble() {
+                force.nodes([]);
+                force.links([]);
+                force.stop();
+                svg.remove();
+                setTimeout(function() {
+                    startBubble();
+                }, 10);
+            }
+
+            function tick() {
+                g.attr("transform", function(d) {
+                    if(isInNewBubblePhase && Math.abs(d.x - middleX) < 10) {
+                        isInNewBubblePhase = false;
+                    }
+                    if(!isInNewBubblePhase && Math.abs(d.x - middleX) > width/4) {
+                        isInNewBubblePhase = true;
+                        setTimeout(function() {
+                            newBubble();
+                        }, 10);
+                    }
+                    return "translate(" + d.x + "," + d.y + ")";
+                })
+            }
+
+            function startBubble() {
+                svg = d3.select("body").append("svg")
+                    .attr("width", width)
+                    .attr("height", height);
+
                 currentVal++;
-                $scope.bubbles = {
+                var bubbles = {
                     nodes: [{name:currentVal, group:0}],
                     links: []
                 };
-                $scope.$apply();
-            }
 
-            $scope.$on('new-bubble', function(evt) {
-                createNextBubble();
-            });
-
-            createNextBubble();
-        }]
-    );
-
-    gameAController.directive('gameA', function() {
-        var link = function(scope, element, attr) {
-            var GET_XY_RE = /translate\((.*)\,(.*)\)/;
-
-            scope.$watch('bubbles', function(bubbles) {
-                if(typeof(bubbles) === 'undefined') {
-                    return;
-                }
-
-                var height = $(window).height(),
-                    width = $(window).width();
-
-                var middleX = width / 2;
-
-                var color = d3.scale.category20();
-
-                var force = d3.layout.force()
+                force = d3.layout.force()
                     .charge(-150)
                     .linkDistance(10)
                     .size([width, height]);
-
-                var svg = d3.select("svg");
-
-                if(svg.empty()) {
-                    svg = d3.select("body").append("svg")
-                        .attr("width", width)
-                        .attr("height", height);
-                }
-                else {
-                    d3.selectAll("svg .bubble").remove();
-                }
 
                 force
                     .nodes(bubbles.nodes)
                     .links(bubbles.links)
                     .start();
 
-                var g = svg.selectAll(".bubble")
+                g = svg.selectAll(".bubble")
                     .data(bubbles.nodes)
                     .enter().append("g")
                     .attr("class", function (d) {
@@ -80,31 +73,16 @@ com_geekAndPoke_Ngm1.gameAController = (function() {
                     .attr("dx", "-.9em")
                     .attr("dy", ".35em");
 
-                force.on("tick", function() {
-                    g.attr("transform", function(d) {
-                        if(isInNewBubblePhase && Math.abs(d.x - middleX) < 10) {
-                            isInNewBubblePhase = false;
-                        }
-                        if(!isInNewBubblePhase && Math.abs(d.x - middleX) > width/4) {
-                            isInNewBubblePhase = true;
-                            force.nodes([]);
-                            force.links([]);
-                            force.stop;
-                            svg.remove();
-                            scope.$parent.$broadcast('new-bubble');
-                        }
-                        return "translate(" + d.x + "," + d.y + ")";
-                    })
-                });
-            });
-        };
+                force.on("tick", tick);
+            }
 
-        var ret = {
-            link: link,
-            restrict: 'E'
-        };
-        return ret;
-    });
+            height = $(window).height();
+            width = $(window).width();
+            middleX = width / 2;
+
+            startBubble();
+        }]
+    );
 
     return gameAController;
 })();
