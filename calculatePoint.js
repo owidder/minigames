c1a9efdb613b902ddba9a317de7e3c0d4b708324
Oliver Widder <oliver.widder@iteratec.de>
2015-04-04 08:37:58 +0200

    calculatePoint

---
+++ calculatePoint.js
@@ -0,0 +1,6 @@
+        function calculatePoint() {
+            if (oldAngle > (Math.PI * 3 / 2) && angle < (Math.PI * 1 / 2)) {
+                pointDisplay.increase();
+            }
+        }
+
